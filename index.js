14644627a202c244ce20dcc23ab5c82d406dbcdc
lichun <740056710@qq.com>
2016-11-21 17:33:08 +0800

    优化

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ let fs = require('fs')
 let path = require('path')
 var app = require('./app').default;
 
-app.listen(3000);
+app.listen(80);
 fs.access(path.join(__dirname, 'config/'), (err)=>{
     if(!err){
         https.createServer({
-            key: fs.readFileSync(path.join(__dirname, 'config/500.key')),
-            cert: fs.readFileSync(path.join(__dirname, 'config/500.crt'))
+            key: fs.readFileSync(path.join(__dirname, 'config/api.wxminapp.com.key')),
+            cert: fs.readFileSync(path.join(__dirname, 'config/api.wxminapp.com.crt'))
         }, app.callback()).listen(443)
     }
 })
