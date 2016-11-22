b094f7a1a40319d1c3094e0a5c7fc5dad3ca173a
lichun <740056710@qq.com>
2016-11-22 17:34:17 +0800

    优化

--- app/tools/webhooks.js
+++ app/tools/webhooks.js
@@ -4,7 +4,7 @@
 const exec = require('child_process').exec;
 export async function pullStaticCode(cb) {
     return new Promise(function (resolve, reject) {
-        exec('git pull /home/project/sampsonapp', function (err, stdout, stderr) {
+        exec('GIT_DIR=/home/project/sampsonapp/.git git pull', function (err, stdout, stderr) {
             if(err){
                 reject(err)
             }else {
