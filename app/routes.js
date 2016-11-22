d61619e761670e666b2ad84c80a56777bf5fb8f6
lichun <740056710@qq.com>
2016-11-22 17:27:37 +0800

    'update'

--- app/routes.js
+++ app/routes.js
@@ -2,15 +2,13 @@
  * Created by lichun on 2016/11/15.
  */
 import Router from 'koa-router'
-import webhooks from './tools/webhooks'
+import {pullStaticCode} from './tools/webhooks'
+
 let route = new Router
-route.post('/webhooks/pullstaticcode', (ctx)=>webhooks.pullStaticCode(function (err, stdout, stderr) {
-    if(err) {
-        ctx.body = err
-    }else {
-        ctx.body = stdout
-    }
-}))
+route.get('/webhooks/pullstaticcode', async (ctx)=>{
+    let stdout = await pullStaticCode()
+    ctx.body = stdout
+})
 route.get('/api/hello', (ctx)=> {
     ctx.body = {success:'I love you'}
 })
--- app/tools/webhooks.js
+++ app/tools/webhooks.js
@@ -2,8 +2,15 @@
  * Created by lichun on 2016/11/22.
  */
 const exec = require('child_process').exec;
-export function pullStaticCode(cb) {
-   exec('git pull /home/project/sampsonapp', function (err, stdout, stderr) {
-       cb(err, stdout, stderr)
-   })
+export async function pullStaticCode(cb) {
+    return new Promise(function (resolve, reject) {
+        exec('git pull /home/project/sampsonapp', function (err, stdout, stderr) {
+            if(err){
+                reject(err)
+            }else {
+                resolve(stdout)
+            }
+        })
+    })
+
 }
