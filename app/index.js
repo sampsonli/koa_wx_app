861dfe7dabb0568ab41eccccd60944ac8343d3fc
sampson <740056710@qq.com>
2016-11-29 21:54:11 +0800

    fix bug

--- app/index.js
+++ app/index.js
@@ -2,16 +2,16 @@ import Koa from 'koa'
 import routing from './routes'
 import logger from 'koa-logger'
 import bodyParser from 'koa-bodyparser'
-import staticCache from 'koa-static-cache'
+import server from 'koa-static'
 import path from 'path'
 import cors from './middleware/cors'
 const app = new Koa();
 app.use(cors)
 app.use(logger())
-// app.use(staticCache(path.join(__dirname, '../static'),{
-app.use(staticCache('/home/project/sampsonapp/dist/',{
+app.use(server(path.join(__dirname, '../../static'),{
+// app.use(server('/home/project/sampsonapp/dist/',{
     gzip: true,
-    maxAge: 60 * 30
+    maxage: 60 * 30
 }))
 app.use(bodyParser())
 routing(app)
