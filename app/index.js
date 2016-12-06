44b432b331759c26d55bff58ddf7091bcf21c059
sampsonli <740056710@qq.com>
2016-12-06 21:20:13 +0800

    fix bug

--- app/index.js
+++ app/index.js
@@ -2,16 +2,16 @@ import Koa from 'koa'
 import routing from './routes'
 import logger from 'koa-logger'
 import bodyParser from 'koa-bodyparser'
-import server from 'koa-static'
+import staticCache from 'koa-static-cache'
 import path from 'path'
 import cors from './middleware/cors'
 const app = new Koa();
 app.use(cors)
 app.use(logger())
-app.use(server(path.join(__dirname, '../../static'),{
+app.use(staticCache(path.join(__dirname, '../../static'),{
     gzip: true,
-    maxage: 1800*1000
-}))
+    maxAge: 60 * 60
+}));
 app.use(bodyParser())
 routing(app)
 export default app
